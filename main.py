b939b25b2a928a7eb871fef719e1cc4ad5d92385
Robert Fish <elasys63+github@gmail.com>
2020-01-04 22:43:45 +1000

    removed bad for loops

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import glob, os
+import glob
+import os
 import shutil
 from variables import path, basePath
 
@@ -6,20 +7,21 @@ print('Welcome to WatchDog Cleanup 🗑')
 print('Running cleanup... 🛠')
 os.chdir(path)
 
-for file in glob.glob("*.zip"):
-    print('Moving file ', file)
-    shutil.move(file, basePath + '/Compactor')
+for file in glob.glob("*"):
+    if file.endswith('.app'):
+        print('Moving file ', file)
+        shutil.move(file, basePath + '/Compactor')
 
-for file in glob.glob("*.app"):
-    print('Moving file ', file)
-    shutil.move(file, basePath + '/Compactor')
+    elif file.endswith('.zip'):
+        print('Moving file ', file)
+        shutil.move(file, basePath + '/Compactor')
 
-for file in glob.glob("*.pdf"):
-    print("Moving file ", file)
-    shutil.move(file, basePath + '/Documents')
+    elif file.endswith('.pdf'):
+        print('Moving file ', file)
+        shutil.move(file, basePath + '/Documents')
 
-for file in glob.glob("*.jpg"):
-    print("Moving file ", file)
-    shutil.move(file, basePath + '/Pictures')
+    elif file.endswith('.jpg'):
+        print('Moving file ', file)
+        shutil.move(file, basePath + '/Pictures')
 
 print('Clean up complete 😃')
